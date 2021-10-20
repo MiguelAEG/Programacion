cfeccefd4fde56dff947ea0315fbf91505e2d39b
Yuue0z <miguel.egk@gmail.com>
2021-10-20 10:53:13 -0500

    Agregue estructuras

--- escuela.c
+++ escuela.c
@@ -2,9 +2,54 @@
 //#include "limpieza.h" 
 //#define fflush(stdin) limpieza()
 
+struct Alumn{
+ long int Matricula;
+ char nombre[40];
+ char carrera[40];
+ int semestre;
+ int fecha_nac;
+ char correo[40];
+ int telefono;
+};
+
+struct materia{
+ int clave;
+ char nombre[40];
+ int semestre;
+};
+
+struct profesor{
+ int num_empleado;
+ char nombre[40];
+ int coordinacion;
+ int fecha_nac;
+ char correo[40];
+ int telefono;
+};
+
+struct fecha_nac{
+ int dia;
+ int mes;
+ int año;
+};
+
+struct grupo{
+ int numero;
+ int semestre;
+ int clave;
+ //Fecha actual
+ int N_empleado;
+};
+
+struct inscripcion{
+ int num;
+ int matricula;
+ //Fecha actual
+ 
+};
 int main()
 {
     
     
     return 0;
-}
+};
